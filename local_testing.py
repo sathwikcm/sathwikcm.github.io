a1b8ef442d9d95665da9da7a77e432536418a9c0
Sathwik <sathwikcm@IBM.com>
2020-06-03 23:44:35 -0400

    improvements

--- local_testing.py
+++ local_testing.py
@@ -1,13 +1,9 @@
 from flask import Flask
 import os
 
-
-app = Flask(__name__, static_url_path='',static_folder='./',)
+app = Flask(__name__, static_url_path='', static_folder='./',)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 port = int(os.getenv("PORT", 8080))
-
-
-
 
 @app.route("/")
 def main():
